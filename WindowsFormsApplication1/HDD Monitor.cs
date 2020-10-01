8c3878afb3f7b992a960de45ec05676fdd19fe05
Kevin Stevens <kevin.stevens@geoslam.com>
2020-10-01 16:33:50 +0100

    activity monitor logic and text corrections

--- WindowsFormsApplication1/HDD Monitor.cs
+++ WindowsFormsApplication1/HDD Monitor.cs
@@ -24,26 +24,27 @@ namespace HDDMonitor
         Icon activeIcon;
         Icon idleIcon;
         Thread hddLedWorker;
-        int bytesSentSpeed = 0;
-        int bytesReceivedSpeed = 0;
         #endregion
 
         #region Hiding Window + Menu Code
 
         public HDDMonitorCode()
         {
+            String title = "JigDisk HDD LED v0.1.1";
             InitializeComponent();
 
             //Load in the icons from files, into their own objects(variables) 
             activeIcon = new Icon("HD_Busy.ico");
             idleIcon = new Icon("HD_Idle.ico");
-            hddLedIcon = new NotifyIcon(); //Creates the actual notification icon for Windows
-            hddLedIcon.Icon = idleIcon; //Set a default icon
-            hddLedIcon.Visible = true; //Make sure it's visisble
-            hddLedIcon.Text = "JigTools v0.1a"; //Displays Speed in ToolTip
+            hddLedIcon = new NotifyIcon
+            {
+                Icon = idleIcon, //Set a default icon
+                Visible = true, //Make sure it's visisble
+                Text = title //Displays Speed in ToolTip
+            }; //Creates the actual notification icon for Windows
 
             //Make a right-click menu for the icon
-            MenuItem progNameMenuItem = new MenuItem("JigDisk HDD Monitor v0.1"); //First menu entry & text
+            MenuItem progNameMenuItem = new MenuItem(title); //First menu entry & text
             MenuItem quitMenuItem = new MenuItem("Quit"); //Second menu entry & text
             ContextMenu contextMenu = new ContextMenu(); //Creates the menu itself
             contextMenu.MenuItems.Add(progNameMenuItem); //Adds the item to the menu
@@ -51,7 +52,7 @@ namespace HDDMonitor
 
             hddLedIcon.ContextMenu = contextMenu; //Links the newly made menu, to the icon in the taskbar
 
-            quitMenuItem.Click += quitMenuItem_Click;  //Creates a function for clicking the Quit item
+            quitMenuItem.Click += QuitMenuItem_Click;  //Creates a function for clicking the Quit item
             progNameMenuItem.Click += ProgNameMenuItem_Click; //Creates a function for clicking the Program Name item
 
             //Need to get rid of the window, as this is application is only for the taskbar
@@ -68,11 +69,11 @@ namespace HDDMonitor
         //What happens when you click the Program Name item
         private void ProgNameMenuItem_Click(object sender, EventArgs e) 
         {            
-            MessageBox.Show("You dirty fucking bastard...", "Anal", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            MessageBox.Show("Thanks for looking!", "Hello", MessageBoxButtons.OK, MessageBoxIcon.Hand);
         }
 
-        //Whath happens when you click the quit menu button
-        private void quitMenuItem_Click(object sender, EventArgs e)
+        //What happens when you click the quit menu button
+        private void QuitMenuItem_Click(object sender, EventArgs e)
         {
             hddLedWorker.Abort();
             hddLedIcon.Dispose();        
@@ -87,43 +88,40 @@ namespace HDDMonitor
         {
 
             ManagementClass driveDataClass = new ManagementClass("Win32_PerfformattedData_PerfDisk_PhysicalDisk");
-            
+            UInt64 total_disk_read_size = 0;
+            UInt64 add_disk_read_size = 0;
+
             try
             {
-
                 //An infinite loop, for monitoring without end
-                        
-                
                 while (true)
                 {
-
+                    total_disk_read_size = 0;
                     ManagementObjectCollection driveDataClassCollection = driveDataClass.GetInstances(); //Access the information on disk activity through WMI
                     foreach( ManagementObject obj in driveDataClassCollection) 
-
                     {
                         if( obj["Name"].ToString() == "_Total") // Target the specific value we want to read (Total)
                         {
-                            if( Convert.ToUInt64(obj["DiskBytesPersec"]) > 0 ) //Pull, convert, and check the bytes being read/written by the disc
+                            add_disk_read_size = Convert.ToUInt64(obj["DiskBytesPersec"]);
+                            if (  add_disk_read_size > 0 ) //Pull, convert, and check the bytes being read/written by the disc
                             {
-                                hddLedIcon.Icon = activeIcon;
-                            }
-                            else
-                            {
-                                hddLedIcon.Icon = idleIcon;
+                                total_disk_read_size += add_disk_read_size;
                             }
                         }
                     }
-
-                    Thread.Sleep(50); //millisecond delay between looping to minimzie CPU usage
+                    if(total_disk_read_size > 0)
+                    {
+                        hddLedIcon.Icon = activeIcon;
+                    } else {
+                        hddLedIcon.Icon = idleIcon;
+                    }
+                    Thread.Sleep(1000/60); //millisecond delay between looping to minimzie CPU usage
                 }
             } catch( ThreadAbortException tbe ) // Cleanup code for when the program is closed
             {
                 driveDataClass.Dispose();
                 //Thread aborted 
             }
-            
-
-
         }
 
         #endregion
